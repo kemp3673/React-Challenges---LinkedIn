744c7660ca57091bbf4d738d55acf2f854c4f930
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-11-21 15:38:04 -0600

    Completed Challenge #8

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { PixelContext } from '../App'
 
 function ColorPicker () {
-  const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
+  const { setSelectedColor } = useContext(PixelContext)
+  const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple'];
   return (
     <div>
       <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} />)}
+      {colors.map(color => <button key={color} style={{ backgroundColor: color }} onClick={() => setSelectedColor(color)}/>)}
     </div>
   )
 }
 
 function Pixel () {
-  return <div style={{ height: '20px', width: '20px', backgroundColor: 'lightGrey', margin: '1px' }} />
+  const { selectedColor } = useContext(PixelContext);
+  const [pixelColor, setPixelColor] = useState('lightGrey')
+  return <div style={{ height: '20px', width: '20px', backgroundColor: `${pixelColor}`, margin: '1px' }} onClick={() => setPixelColor(selectedColor)}/>
 }
 
 function Pixels () {
+
   const pixels = []
   for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />)
   return (
--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css'
+import { useState, useContext, createContext } from 'react';
 import PixelArt from './08-pixel-art/PixelArt'
 
+export const PixelContext = createContext(null);
+
 function App () {
+  const [selectedColor, setSelectedColor] = useState({color: 'lightGrey'});
+
   return (
+    <PixelContext.Provider value={{selectedColor, setSelectedColor}}>
     <div className='App'>
       <PixelArt />
     </div>
+    </PixelContext.Provider>
   )
 }
 
