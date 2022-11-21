f2751471ca073ae018a3137a6e7b206601fc31d6
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-11-21 01:15:23 -0600

    Completed challenge #3, added regex email validation

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -1,12 +1,37 @@
 import { useState } from 'react'
 
 export default function FormValidator () {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [message, setMessage] = useState(null);
+
+  const validation = (event) => {
+    event.preventDefault();
+    const validEmail = email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+
+    if (email === '' || password === '' || passwordConfirm === '') {
+      setMessage('All fields must be filled in');
+      return;
+    }
+    else if (!validEmail) {
+      setMessage('Invalid Email Format');
+      return;
+    } else if (password.length !== 8) {
+      setMessage('Password must be minimum 8 characters');
+      return;
+    } else if (password !== passwordConfirm) {
+      setMessage('Passwords do not match');
+      return;
+    } else {
+      setMessage('Successfully Signed Up');
+      return;
+    }
+
+  };
 
   return (
-    <form>
+    <form onSubmit={(e) => validation(e)}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
@@ -23,7 +48,8 @@ export default function FormValidator () {
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
       />
-      <input type='submit' value='Submit' />
+      <p>{message}</p>
+      <input type='submit' value='Submit'/>
     </form>
   )
 }
