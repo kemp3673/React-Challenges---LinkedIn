69fc8c8430a524376852e6d9dd070d352a412087
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-11-21 02:03:53 -0600

    Compeleted Challenge #6

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -3,10 +3,11 @@ import WindowEvent from './WindowEvent'
 
 export default function ToggleWindowEvent () {
   const [windowEvent, setWindowEvent] = useState(false)
+
   return (
     <div>
       <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
-      {windowEvent && <WindowEvent />}
+      {windowEvent ? <WindowEvent /> : null}
     </div>
   )
 }
--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,16 @@
+import { useEffect } from 'react'
+
 export default function WindowEvent () {
+
+
+  useEffect(() => {
+    const doubleClick = () => {
+      alert('Double Click Detected');
+    }
+    window.addEventListener('dblclick', doubleClick)
+    return () => window.removeEventListener('dblclick', doubleClick)
+  }, [])
+
   return (
     <h2>Window event active</h2>
   )
