ae828f397c5b53c326191929dc5be79eb7c38cde
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-11-21 01:30:26 -0600

    Completed challenge #4

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,27 @@
-export default function DogPics () {
+import { useState, useEffect } from "react";
+
+export default function DogPics() {
+  const [dog, setDog] = useState(
+    "https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg"
+  );
+
+  const getDoggieImage = async () => {
+    await fetch("https://dog.ceo/api/breeds/image/random")
+    .then((data) => data.json())
+    .then((image) => {
+      setDog(image.message);
+    })
+    .catch((err) => console.error("Error Retrieving Dog Image", err));
+  }
+
+  useEffect(() => {
+    getDoggieImage();
+  }, []);
   // API: https://dog.ceo/dog-api/
   return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+    <div className="dog-pics">
+      <img src={dog} />
+      <button onClick={() => getDoggieImage()}>🐶</button>
     </div>
-  )
+  );
 }
