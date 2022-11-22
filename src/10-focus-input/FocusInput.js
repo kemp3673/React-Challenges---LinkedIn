aacb2b73b4b1f82cf12ef20ec0e03dd6404e74ff
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-11-22 00:46:56 -0600

    Compeleted challenge #10

--- src/10-focus-input/FocusInput.js
+++ src/10-focus-input/FocusInput.js
@@ -1,8 +1,22 @@
+import { useRef, useEffect } from 'react';
+
 export default function FocusInput() {
+  const focusedInput = useRef(null);
+
+  useEffect(() => {
+    focusedInput.current.focus();
+  }, [])
+
   return (
     <div>
       <label htmlFor='focused-input'>Focus me on page load!</label>
-      <input name='focused-input'></input>
+      <input
+        name='focused-input'
+        ref={focusedInput}
+      >
+      </input>
+      {/* <input autoFocus name='focused-input'></input> */}
+      {/* This 'autofocus works pretty much the same but does not require use of refs */}
     </div>
   )
 }
