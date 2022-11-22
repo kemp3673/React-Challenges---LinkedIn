ef40a4868cde313dca58258cb1f72cc661eac122
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-11-22 01:29:28 -0600

    Completed challenge #10

--- src/11-shopping-cart/ShoppingCart.js
+++ src/11-shopping-cart/ShoppingCart.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const items = [{
   name: 'apple',
@@ -12,7 +12,35 @@ const items = [{
 }]
 
 function ShoppingCart () {
-  const cart = [{ name: 'apple', quantity: 3, price: 0.39 }]
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const updateQuantity = (index, action) => {
+    let stateCopy = [...cart];
+    if (action === 'ADD') {stateCopy[index].quantity++};
+    if (action === 'MINUS') {stateCopy[index].quantity--};
+    if (stateCopy[index].quantity < 1) {
+      stateCopy.splice(index, 1);
+    }
+    setCart(stateCopy);
+  };
+
+  const addToCart = (item) => {
+    for (var i = 0; i < cart.length; i++) {
+      if (cart[i].name === item.name) {
+        updateQuantity(i, 'ADD');
+        return;
+      }
+    }
+    setCart([...cart, {...item, quantity: 1}])
+  }
+
+  useEffect(() => {
+    let total = 0;
+    cart.map(item => total += (item.quantity * item.price));
+    setTotal(total);
+  }, [cart])
+
 
   return (
     <div>
@@ -24,27 +52,27 @@ function ShoppingCart () {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>${item.price}</p>
-              <button>Add to Cart</button>
+              <button onClick={() => addToCart(item)}>Add to Cart</button>
             </div>)
           )}
         </div>
         <div>
           <h2>Cart</h2>
-          {cart.map(item => (
+          {cart.map((item, index) => (
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>
-                <button>-</button>
+                <button onClick={() => updateQuantity(index, 'MINUS')}>-</button>
                 {item.quantity}
-                <button>+</button>
+                <button onClick={() => updateQuantity(index, 'ADD')}>+</button>
               </p>
-              <p>Subtotal: ${item.quantity * item.price}</p>
+              <p>Subtotal: ${(item.quantity * item.price).toFixed(2)}</p>
             </div>
           ))}
         </div>
       </div>
       <div className='total'>
-        <h2>Total: $0.00</h2>
+        <h2>Total: ${total.toFixed(2)}</h2>
       </div>
     </div>
   )
